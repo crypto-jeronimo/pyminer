3689de545b2b204fdc09f13c1e67d2a990c99e48
暗号ジェロニモ Jerônimo Cryptão <crypto.jeronimo@gmail.com>
2018-03-17 11:53:49 +0000

    Update build script.

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
-scrypt:
+deps:
+	sudo apt-get install -y python2.7-dev
+
+install:
 	python setup.py build_ext --inplace
+	rm -rf $(shell pwd)/build
+
+deps-arm:
+	pkg instll -y python2-dev
+
+install-arm:
+	PLATFORM=arm python2 setup.py build_ext --inplace
 	rm -rf $(shell pwd)/build
 
 clean:
--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import os
 from distutils.core import setup, Extension
 
+args = ['-O3']
+if os.environ.get('PLATFORM', '') == 'ARM':
+    args += ['-funroll-loops', '-fomit-frame-pointer']
+
 scrypt_module = Extension('scrypt',
-                               sources = ['scryptmodule.c',
-                                          'scrypt.c'],
-                               include_dirs=['.'])
+                           sources = ['scryptmodule.c',
+                                      'scrypt.c'],
+                           extra_compile_args=args,
+                           include_dirs=['.'])
 
 setup(name='scrypt',
        version='1.0',
