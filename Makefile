3a294fa84088113c6ef7b12ece46a25e7cc1b325
暗号ジェロニモ Jerônimo Cryptão <crypto.jeronimo@gmail.com>
2018-03-18 18:37:25 +0000

    Update makefile and unify `setup.py` files.

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 deps:
 	sudo apt-get install -y python2.7-dev
 
-deps-android:
+deps-android-gnuroot:
+	apt install -y python2.7-dev
+
+deps-android-termux:
 	pkg instll -y python2-dev
 
 install:
@@ -12,9 +15,13 @@ install-arm:
 	PLATFORM=arm python setup.py build_ext --inplace
 	rm -rf $(shell pwd)/build
 
-install-android:
+install-android-termux:
+	python2.7 setup.py build_ext --inplace
+	rm -rf $(shell pwd)/build
+
+install-android-termux:
 	PLATFORM=arm python2 setup.py build_ext --inplace
 	rm -rf $(shell pwd)/build
 
 clean:
-	rm -f $(shell pwd)/scrypt.so
+	rm -f $(shell pwd)/*.so
---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, Extension
+
+
+extra_compile_args = (
+                      ['-O3', '-funroll-loops', '-fomit-frame-pointer']
+                      if os.environ.get('PLATFORM', '') == 'arm'
+                      else ['-O3'])
+
+scrypt_module = Extension('scrypt',
+                           sources=['./algos/scrypt/scryptmodule.c',
+                                    './algos/scrypt/scrypt.c'],
+                           extra_compile_args=extra_compile_args,
+                           include_dirs=['./algos/scrypt'])
+
+yescrypt_module = Extension('yescrypt',
+                            sources = ['./algos/yescrypt/yescrypt.c'],
+                            extra_compile_args=extra_compile_args,
+                            include_dirs=['./algos/yescrypt'])
+
+setup(name='scrypt',
+       version='1.0',
+       description='Bindings for scrypt proof of work',
+       ext_modules=[scrypt_module])
+
+setup(name='yescrypt',
+       version='1.0',
+       description='Bindings for yescrypt proof of work',
+       ext_modules=[yescrypt_module])
